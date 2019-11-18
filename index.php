37064b1d15a3ae5b97555635d24421e9fab54025
beaisobelle <54380932+beaisobelle@users.noreply.github.com>
2019-11-17 23:37:13 -0500

    Update index.php

--- index.php
+++ index.php
@@ -10,21 +10,37 @@ if($user->is_logged_in()) {
 }
 ?>
 
+
 <html>
   <head>
     <title>Campus Snapshot</title>
+    <link rel="stylesheet" type="text/css" href="tpl/css/index.css">
   </head>
 
   <body>
+    <div class="wrapper">
+      <nav id="header" style="height: 100px;">
+         <div class="wrapper">
+            <div class="logo">
+                <img class="logo-fau" src="tpl/css/index-images/campus_snapshot5.png" width="1000" height="200">
+            </div>
+         </div>
+    </nav> 
 
   <form method="post" action="includes/process_login.php">
-      Username: <input type="text" name="username" />
-      <br />
-      Password: <input type="password" name="password" />
-      <br />
-
-      <input type="submit" value="Login">
+    <div class="boxed">
+        Username: <input type="text" name="username" />
+        <br />
+    </div>
+     <div class="boxed2">
+        Password: <input type="password" name="password" />
+        <br />
+    </div>
+      <div class="login">
+        <input type="submit" value="Login" width="20" length="30">
+    </div>
   </form>
+      </div>
 
   </body>
   </html>
