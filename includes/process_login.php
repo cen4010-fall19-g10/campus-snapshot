94240eae9950c4148c8cf577073838348f8066bc
beaisobelle <54380932+beaisobelle@users.noreply.github.com>
2019-11-17 23:37:06 -0500

    Update process_login.php

--- includes/process_login.php
+++ includes/process_login.php
@@ -2,10 +2,10 @@
 session_start();
 
 if(!isset($_POST['username']) || !isset($_POST['password']))
-  die('Bad');
+  die('Your incredentials are incorrect. Please try again.');
 
 if(empty($_POST['username']) || empty($_POST['password']))
-  die('Bad');
+  die('You did not input your credentials. Please try again.');
 
 include("Database/Database.class.php");
 include("User/User.class.php");
